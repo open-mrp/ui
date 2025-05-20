b2f2a0e7711b90770201acdb1def87607ef275d2
Dane Albaugh <dane.albaugh@gmail.com>
2025-05-20 09:43:15 -0400

    fix: adding missing colors

--- src/buttons/Button.tsx
+++ src/buttons/Button.tsx
@@ -58,6 +58,110 @@ const colorVariants = {
     text: "text-gray-500 hover:bg-gray-500/5",
     icon: "text-gray-500 hover:bg-gray-500/5",
   },
+  black: {
+    contained: "bg-black text-white hover:bg-black/90",
+    outlined: "border-black text-black hover:bg-black/5 hover:border-black",
+    text: "text-black hover:bg-black/5",
+    icon: "text-black hover:bg-black/5",
+  },
+  red: {
+    contained: "bg-red-500 text-white hover:bg-red-600",
+    outlined:
+      "border-red-500 text-red-500 hover:bg-red-500/5 hover:border-red-500",
+    text: "text-red-500 hover:bg-red-500/5",
+    icon: "text-red-500 hover:bg-red-500/5",
+  },
+  yellow: {
+    contained: "bg-yellow-500 text-white hover:bg-yellow-600",
+    outlined:
+      "border-yellow-500 text-yellow-500 hover:bg-yellow-500/5 hover:border-yellow-500",
+    text: "text-yellow-500 hover:bg-yellow-500/5",
+    icon: "text-yellow-500 hover:bg-yellow-500/5",
+  },
+  orange: {
+    contained: "bg-orange-500 text-white hover:bg-orange-600",
+    outlined:
+      "border-orange-500 text-orange-500 hover:bg-orange-500/5 hover:border-orange-500",
+    text: "text-orange-500 hover:bg-orange-500/5",
+    icon: "text-orange-500 hover:bg-orange-500/5",
+  },
+  pink: {
+    contained: "bg-pink-500 text-white hover:bg-pink-600",
+    outlined:
+      "border-pink-500 text-pink-500 hover:bg-pink-500/5 hover:border-pink-500",
+    text: "text-pink-500 hover:bg-pink-500/5",
+    icon: "text-pink-500 hover:bg-pink-500/5",
+  },
+  teal: {
+    contained: "bg-teal-500 text-white hover:bg-teal-600",
+    outlined:
+      "border-teal-500 text-teal-500 hover:bg-teal-500/5 hover:border-teal-500",
+    text: "text-teal-500 hover:bg-teal-500/5",
+    icon: "text-teal-500 hover:bg-teal-500/5",
+  },
+  cyan: {
+    contained: "bg-cyan-500 text-white hover:bg-cyan-600",
+    outlined:
+      "border-cyan-500 text-cyan-500 hover:bg-cyan-500/5 hover:border-cyan-500",
+    text: "text-cyan-500 hover:bg-cyan-500/5",
+    icon: "text-cyan-500 hover:bg-cyan-500/5",
+  },
+  lime: {
+    contained: "bg-lime-500 text-white hover:bg-lime-600",
+    outlined:
+      "border-lime-500 text-lime-500 hover:bg-lime-500/5 hover:border-lime-500",
+    text: "text-lime-500 hover:bg-lime-500/5",
+    icon: "text-lime-500 hover:bg-lime-500/5",
+  },
+  indigo: {
+    contained: "bg-indigo-500 text-white hover:bg-indigo-600",
+    outlined:
+      "border-indigo-500 text-indigo-500 hover:bg-indigo-500/5 hover:border-indigo-500",
+    text: "text-indigo-500 hover:bg-indigo-500/5",
+    icon: "text-indigo-500 hover:bg-indigo-500/5",
+  },
+  violet: {
+    contained: "bg-violet-500 text-white hover:bg-violet-600",
+    outlined:
+      "border-violet-500 text-violet-500 hover:bg-violet-500/5 hover:border-violet-500",
+    text: "text-violet-500 hover:bg-violet-500/5",
+    icon: "text-violet-500 hover:bg-violet-500/5",
+  },
+  rose: {
+    contained: "bg-rose-500 text-white hover:bg-rose-600",
+    outlined:
+      "border-rose-500 text-rose-500 hover:bg-rose-500/5 hover:border-rose-500",
+    text: "text-rose-500 hover:bg-rose-500/5",
+    icon: "text-rose-500 hover:bg-rose-500/5",
+  },
+  amber: {
+    contained: "bg-amber-500 text-white hover:bg-amber-600",
+    outlined:
+      "border-amber-500 text-amber-500 hover:bg-amber-500/5 hover:border-amber-500",
+    text: "text-amber-500 hover:bg-amber-500/5",
+    icon: "text-amber-500 hover:bg-amber-500/5",
+  },
+  emerald: {
+    contained: "bg-emerald-500 text-white hover:bg-emerald-600",
+    outlined:
+      "border-emerald-500 text-emerald-500 hover:bg-emerald-500/5 hover:border-emerald-500",
+    text: "text-emerald-500 hover:bg-emerald-500/5",
+    icon: "text-emerald-500 hover:bg-emerald-500/5",
+  },
+  fuchsia: {
+    contained: "bg-fuchsia-500 text-white hover:bg-fuchsia-600",
+    outlined:
+      "border-fuchsia-500 text-fuchsia-500 hover:bg-fuchsia-500/5 hover:border-fuchsia-500",
+    text: "text-fuchsia-500 hover:bg-fuchsia-500/5",
+    icon: "text-fuchsia-500 hover:bg-fuchsia-500/5",
+  },
+  sky: {
+    contained: "bg-sky-500 text-white hover:bg-sky-600",
+    outlined:
+      "border-sky-500 text-sky-500 hover:bg-sky-500/5 hover:border-sky-500",
+    text: "text-sky-500 hover:bg-sky-500/5",
+    icon: "text-sky-500 hover:bg-sky-500/5",
+  },
 } as const;
 
 const buttonVariants = cva(BASE_STYLES, {
